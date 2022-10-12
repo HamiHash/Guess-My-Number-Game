f37a57ea2db4342e691ada588125e2fded0436b0
HamiHash <hami.hash@yahoo.com>
2022-10-12 22:30:06 +0330

    Refactoring
    Code is cleaner now whit some few changes.

--- script.js
+++ script.js
@@ -12,45 +12,68 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
+
+// Refactoring the document.queryselector..... for message to make the code cleaner.
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
   // NO INPUT
   if (!guess) {
-    document.querySelector(".message").textContent = "No Number!";
+    displayMessage("No Number!");
 
+    // When Guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? "Too high!" : "Too low!");
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      displayMessage("You lost the game!");
+      document.querySelector(".score").textContent = 0;
+    }
     // When Player Wins
   } else if (guess === secretNumber) {
-    document.querySelector(".message").textContent = "Correct Number!";
+    displayMessage("Correct Number!");
     document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
 
     document.querySelector(".number").style.width = "30rem";
 
-    // When Guess is too HIGH
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = "Too high!";
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".message").textContent = "You lost the game!";
-      document.querySelector(".score").textContent = 0;
-    }
-
-    // When Guess is too LOW
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = "Too low!";
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".message").textContent = "You lost the game!";
-      document.querySelector(".score").textContent = 0;
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
     }
   }
 });
+
+// this is the first code wich was not cool becouse its too long:
+
+// When Guess is too HIGH
+// } else if (guess > secretNumber) {
+//   if (score > 1) {
+//     document.querySelector(".message").textContent = "Too high!";
+//     score--;
+//     document.querySelector(".score").textContent = score;
+//   } else {
+//     document.querySelector(".message").textContent = "You lost the game!";
+//     document.querySelector(".score").textContent = 0;
+//   }
+
+//   // When Guess is too LOW
+// } else if (guess < secretNumber) {
+//   if (score > 1) {
+//     document.querySelector(".message").textContent = "Too low!";
+//     score--;
+//     document.querySelector(".score").textContent = score;
+//   } else {
+//     document.querySelector(".message").textContent = "You lost the game!";
+//     document.querySelector(".score").textContent = 0;
+//   }
 
 document.querySelector(".again").addEventListener("click", function () {
   score = 20;
@@ -59,6 +82,6 @@ document.querySelector(".again").addEventListener("click", function () {
   document.querySelector("body").style.backgroundColor = "#222";
   document.querySelector(".guess").value = "";
   document.querySelector(".score").textContent = score;
-  document.querySelector(".message").textContent = "Start guessing...";
+  displayMessage("Start guessing...");
   document.querySelector(".number").style.width = "15rem";
 });
