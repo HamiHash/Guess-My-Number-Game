4ab14898d1d5a6d391c3dd7bb0c39c942323ac3c
HamiHash <hami.hash@yahoo.com>
2022-10-12 16:21:08 +0330

    Correct or wrong guess Added

--- script.js
+++ script.js
@@ -18,10 +18,18 @@ document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
+  // NO INPUT
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number!';
+
+    // When Player Wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    // When Guess is too HIGH
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high!';
@@ -31,6 +39,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
+
+    // When Guess is too LOW
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too low!';
